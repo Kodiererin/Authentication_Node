ab6e0360199992601adc0bc542afe408f43010f0
Kodiererin <iamujjwal45@gmail.com>
2023-02-21 12:00:44 +0530

    Adding Files

---
+++ auth_level3.js
@@ -0,0 +1,111 @@
+// This is Level 3 Encryotion.
+//////////// Hashing Method //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+require('dotenv').config(); // Installed npm i dotenv
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const app = express();
+
+
+app.use(express.static('public'));
+app.set('view engine' , 'ejs');
+app.use(bodyParser.urlencoded({
+    extended : true
+}));
+const mongoose = require('mongoose');
+
+// Acquiring MD5 for Hashing Encryption
+const md5 = require('md5');
+
+
+
+main().catch(err => console.log(err));
+
+console.log(process.env.UJJWAL);
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/Login');
+  console.log("Database Connected");
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+};
+
+
+mongoose.set('strictQuery' , true);
+
+const userSchema = new mongoose.Schema({
+    email : {
+        type : String,
+    },
+    password : {
+        type : String,
+    }
+});
+
+// const secret = "This is a Little Secret"; this has been shifted to .env file
+
+const user = new mongoose.model('user',userSchema);
+
+
+app.get('/',function(req,res){
+    res.render('home');
+})
+
+app.get('/login',function(req,res){
+    res.render('login');
+})
+
+app.get('/register',function(req,res){
+    res.render('register');
+})
+
+app.post('/register',function(req,res){
+    const newUser = new user({
+        email : req.body.username,
+        password : md5(req.body.password),           // changes has been done here
+    });
+
+    console.log(newUser);
+
+    newUser.save(function(err,result){
+        if(!err && result){
+            console.log('Registration successful');
+            // res.render('login')
+            res.render('secrets');
+        }
+    })
+})
+
+
+app.post('/login',function(req,res){
+    const username = req.body.username;
+    const password = md5(req.body.password);
+    console.log(username+"    "+password);
+    user.findOne(
+        {email : username},
+        function(err,found){
+            if(!err && found){
+                if(found.password===password){
+                    console.log("Logged in Successfully");
+                    res.render('secrets');
+                }
+                else{
+                    console.log("Password Not Matched");
+                }
+            }
+            else{
+                console.log("Data Not Found");
+            }
+        }
+    )
+})
+
+
+
+
+app.listen(3000,function(){
+    console.log("Server Connected to Port 3000");
+})
+
+
---
+++ auth_level6.js
@@ -0,0 +1,136 @@
+// This is Level 5 Encryotion.
+//////////// Using Passport JS //////////////////////////////////////////////////
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const session = require('express-session');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
+
+const app = express();
+
+
+app.use(express.static('public'));
+app.set('view engine' , 'ejs');
+app.use(bodyParser.urlencoded({
+    extended : true
+}));
+// Setting passport packages.
+app.use(session({
+    secret : "Our Little Secret.",
+    resave : false,
+    saveUninitialized:false,
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+main().catch(err => console.log(err));
+
+console.log(process.env.UJJWAL);
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/Login');
+  console.log("Database Connected");
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+};
+mongoose.set('strictQuery' , true);
+// mongoose.set('usecreatindex',true);
+
+
+
+
+const userSchema = new mongoose.Schema({
+    email : {
+        type : String,
+    },
+    password : {
+        type : String,
+    }
+});
+
+userSchema.plugin(passportLocalMongoose);           // to hashpasswords.
+
+// const secret = "This is a Little Secret"; this has been shifted to .env file
+
+const user = new mongoose.model('user',userSchema);
+
+passport.use(user.createStrategy());
+
+
+passport.serializeUser(user.serializeUser());
+passport.deserializeUser(user.deserializeUser());
+
+
+
+app.get('/',function(req,res){
+    res.render('home');
+})
+
+app.get('/login',function(req,res){
+    res.render('login');
+})
+
+app.get('/register',function(req,res){
+    res.render('register');
+})
+
+app.get('/secrets' , function(req,res){
+    // checking if the user is logged in or not (by cookies)
+    if(req.isAuthenticated()){
+        res.render('secrets');
+    }
+
+    else{
+        res.redirect("/login");
+    }
+})
+
+app.post('/register',function(req,res){
+    // using passport local package to use it.
+    user.register({username : req.body.username}, req.body.password , function(err,user){
+        if(err){
+            console.log(err);
+            res.redirect('/register');
+        }else{
+            // we are using the type of authentication called local
+            // authrntication will be successful if we manages to set up the cookie that saved their logged in sesstion
+            // and we have to check if they are logged in or not 
+            passport.authenticate("local")(req,res , function(){
+                res.redirect('/secrets');
+            })
+        }
+    });
+
+})
+
+
+app.post('/login',function(req,res){
+    const newUser = new user({
+        email : req.body.username,
+        password : req.body.pasword
+    })
+
+    // using login function of passport
+    req.logIn(newUser , function(err){
+        if(err){
+            console.log(err);
+        }else{
+            passport.authenticate("local")(req,res,function(){
+                res.redirect('/secrets');
+            })
+        }
+    })
+
+})
+
+
+
+
+app.listen(3000,function(){
+    console.log("Server Connected to Port 3000");
+})
+
+
